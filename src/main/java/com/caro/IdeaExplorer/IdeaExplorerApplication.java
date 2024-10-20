3d21785f19e966e90221555be9193910a1925d58
GameJamCaro <caro@codeforjoy.de>
2024-10-20 18:01:36 +0200

    Init

---
+++ src/main/java/com/caro/IdeaExplorer/IdeaExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.caro.IdeaExplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaExplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caro/IdeaExplorer/IdeaExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caro.IdeaExplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
